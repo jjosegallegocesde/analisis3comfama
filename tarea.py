dff56650b0ac94bac848b442dafa733db3acb3f5
106 <106@academicos.cesde.edu.co>
2023-02-08 11:47:24 -0500

    se crea tarea

---
+++ tarea.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+data = pd.read_csv("./empleados.csv")
+
+'''
+Filtrar empleados que solo sean analistas 1
+Filtrar empleados que solo sean analistas 2
+Filtrar empleados en general que tengan menos de 30 años
+Filtrar empleados en general que tengan mas de 50 años
+¿Cuál es el promedio de salario de un analista 1?
+¿Cuál es el promedio de salario de un analista 2?
+Filtrar empleados cuto porcentaje de deducción sea mayor al 10%
+Cambiar todos los datos nan por el valor 0
+Cambiar los nan de nombres por la palabra default, los nan de cargo por el mensaje sin cargo y edad por 0
+Versionar el desarrollo del taller
+
+'''
+
